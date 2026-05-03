aec7014e34473051302c4b22156bca99eae12c47
machiav3lli <antonios.hazim@tutanota.com>
2026-05-03 18:17:59 +0200

    Update: Revamp PrefDelegate to replace Pref

--- src/main/java/com/machiav3lli/backup/data/preferences/NeoPref.kt
+++ src/main/java/com/machiav3lli/backup/data/preferences/NeoPref.kt
@@ -4,11 +4,21 @@ import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
+import com.machiav3lli.backup.ui.compose.component.BooleanPreference
+import com.machiav3lli.backup.ui.compose.component.EnumPreference
+import com.machiav3lli.backup.ui.compose.component.IntPreference
+import com.machiav3lli.backup.ui.compose.component.ListPreference
+import com.machiav3lli.backup.ui.compose.component.StringEditPreference
+import com.machiav3lli.backup.ui.compose.component.StringPreference
+import com.machiav3lli.backup.ui.compose.component.StringSetPreference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -17,55 +27,278 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
-open class PrefBoolean(
-    @StringRes titleId: Int = -1,
-    @StringRes summaryId: Int = -1,
-    dataStore: DataStore<Preferences>,
-    key: Preferences.Key<Boolean>,
-    defaultValue: Boolean = false,
-    onChange: (Boolean) -> Unit = {}
-) : PrefDelegate<Boolean>(titleId, summaryId, dataStore, key, defaultValue, onChange)
+typealias PrefUI = @Composable (pref: PrefDelegate<out Any>, onDialogPref: (PrefDelegate<out Any>) -> Unit, index: Int, groupSize: Int) -> Unit
 
-open class PrefInt(
+class PrefBoolean(
+    dataStore: DataStore<Preferences>,
+    dataStoreKey: Preferences.Key<Boolean>,
+    defaultValue: Boolean,
     @StringRes titleId: Int = -1,
     @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = false,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefDelegate<Boolean>(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        BooleanPreference(pref = pref as PrefBoolean, index = index, groupSize = groupSize)
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChange = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
+
+class PrefInt(
     dataStore: DataStore<Preferences>,
-    key: Preferences.Key<Int>,
-    val defaultValue: Int = 0,
+    dataStoreKey: Preferences.Key<Int>,
+    defaultValue: Int,
+    @StringRes titleId: Int = -1,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = false,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
     val entries: List<Int>,
-    onChange: (Int) -> Unit = {}
-) : PrefDelegate<Int>(titleId, summaryId, dataStore, key, defaultValue, onChange)
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefDelegate<Int>(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        IntPreference(pref = pref as PrefInt, index = index, groupSize = groupSize)
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChange = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
 
-class StringSetPref(
+open class PrefString(
+    private: Boolean = false,
+    defaultValue: String,
     @StringRes titleId: Int = -1,
     @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
     dataStore: DataStore<Preferences>,
-    val defaultValue: Set<String> = emptySet(),
-    val key: Preferences.Key<Set<String>>,
-    onChange: (Set<String>) -> Unit = {},
-) : PrefDelegate<Set<String>>(titleId, summaryId, dataStore, key, defaultValue, onChange)
+    dataStoreKey: Preferences.Key<String>,
+) : PrefDelegate<String>(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        StringPreference(
+            pref = pref as PrefString,
+            index = index,
+            groupSize = groupSize,
+            onClick = { onDialogUI(pref) },
+        )
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChange = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
+
+class PrefEditString(
+    dataStore: DataStore<Preferences>,
+    dataStoreKey: Preferences.Key<String>,
+    defaultValue: String,
+    @StringRes titleId: Int = -1,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = true,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefString(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, _, index, groupSize ->
+        StringEditPreference(pref = pref as PrefEditString, index = index, groupSize = groupSize)
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChanged = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
+
+class PrefList(
+    dataStore: DataStore<Preferences>,
+    dataStoreKey: Preferences.Key<String>,
+    defaultValue: String,
+    @StringRes titleId: Int = -1,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = false,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    val entries: Map<String, String>,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefString(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        ListPreference(
+            pref = pref as PrefList,
+            index = index,
+            groupSize = groupSize,
+            onClick = { onDialogUI(pref) },
+        )
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChanged = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
+
+class PrefStringSet(
+    dataStore: DataStore<Preferences>,
+    dataStoreKey: Preferences.Key<Set<String>>,
+    defaultValue: Set<String> = emptySet(),
+    @StringRes titleId: Int = -1,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = false,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefDelegate<Set<String>>(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        StringSetPreference(pref = pref as PrefStringSet, index = index, groupSize = groupSize)
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChange = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
+
+class PrefEnum(
+    dataStore: DataStore<Preferences>,
+    dataStoreKey: Preferences.Key<Int>,
+    defaultValue: Int,
+    @StringRes titleId: Int = -1,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    private: Boolean = false,
+    UI: PrefUI? = null,
+    icon: ImageVector? = null,
+    iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    val entries: Map<Int, Int>,
+    enableIf: (() -> Boolean)? = null,
+    onChanged: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
+) : PrefDelegate<Int>(
+    private = private,
+    defaultValue = defaultValue,
+    titleId = titleId,
+    summaryId = summaryId,
+    summary = summary,
+    UI = UI ?: { pref, onDialogUI, index, groupSize ->
+        EnumPreference(
+            pref = pref as PrefEnum,
+            index = index,
+            groupSize = groupSize,
+            onClick = { onDialogUI(pref) },
+        )
+    },
+    icon = icon,
+    iconTint = iconTint,
+    enableIf = enableIf,
+    onChange = onChanged,
+    dataStore = dataStore,
+    dataStoreKey = dataStoreKey,
+)
 
 abstract class PrefDelegate<T : Any>(
-    @StringRes var titleId: Int = -1,
-    @StringRes var summaryId: Int = -1,
     private val dataStore: DataStore<Preferences>,
-    private val key: Preferences.Key<T>,
-    private val defaultValue: T,
-    val onChange: (T) -> Unit
+    private val dataStoreKey: Preferences.Key<T>,
+    val defaultValue: T,
+    @StringRes val titleId: Int,
+    @StringRes val summaryId: Int,
+    var summary: String? = null,
+    val private: Boolean = false,
+    val icon: ImageVector? = null,
+    var iconTint: ((PrefDelegate<out Any>) -> Color)? = null,
+    val UI: PrefUI? = null,
+    val enableIf: (() -> Boolean)? = null,
+    val onChange: (suspend (PrefDelegate<out Any>) -> Unit)? = null,
 ) {
+    val key: String = run {
+        val parts = dataStoreKey.name.split(".", limit = 2)
+        if (parts.size == 2) parts[1] else parts[0]
+    }
+
+    val group: String = run {
+        val parts = dataStoreKey.name.split(".", limit = 2)
+        parts.getOrElse(0) { "" }
+    }
+
+    val dirty = mutableStateOf(false)
+
     private val flow: Flow<T> = dataStore.data
         .catch { exception ->
             if (exception is IOException) emit(emptyPreferences())
             else throw exception
         }
         .map { preferences ->
-            preferences[key] ?: defaultValue
+            preferences[dataStoreKey] ?: defaultValue
         }
         .distinctUntilChanged()
 
-    val value: T
+    var value: T
         get() = runBlocking(Dispatchers.IO) {
             currentValue()
+        }
+        set(value) = runBlocking(Dispatchers.IO) {
+            set(value)
         }
 
     val state: State<T>
@@ -76,11 +309,19 @@ abstract class PrefDelegate<T : Any>(
 
     suspend fun set(newValue: T) {
         if (newValue == currentValue()) return
-        dataStore.edit { prefs -> prefs[key] = newValue }
-        onChange(newValue)
+        dataStore.edit { prefs -> prefs[dataStoreKey] = newValue }
+        onChange?.invoke(this)
     }
 
     private suspend fun currentValue(): T {
-        return dataStore.data.first()[key] ?: defaultValue
+        return flow.first()
     }
+
+    suspend fun clear() {
+        dataStore.edit { prefs -> prefs.remove(dataStoreKey) }
+    }
+
+    //init {
+    //    Pref.prefGroups.getOrPut(group) { mutableListOf() }.add(NeoPrefAdapter(this))
+    //}
 }
--- src/main/java/com/machiav3lli/backup/data/preferences/NeoPrefs.kt
+++ src/main/java/com/machiav3lli/backup/data/preferences/NeoPrefs.kt
@@ -28,205 +28,205 @@ class NeoPrefs private constructor(val context: Context) : KoinComponent {
 
     val sortHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.SORT_HOME,
+        dataStoreKey = PrefKey.SORT_HOME,
         defaultValue = Sort.LABEL.ordinal,
         entries = Sort.entries.map { it.ordinal },
     )
 
     val sortBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.SORT_BACKUP,
+        dataStoreKey = PrefKey.SORT_BACKUP,
         defaultValue = Sort.LABEL.ordinal,
         entries = Sort.entries.map { it.ordinal },
     )
 
     val sortRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.SORT_RESTORE,
+        dataStoreKey = PrefKey.SORT_RESTORE,
         defaultValue = Sort.LABEL.ordinal,
         entries = Sort.entries.map { it.ordinal },
     )
 
     val sortAscHome = PrefBoolean(
         dataStore = dataStore,
-        key = PrefKey.SORT_ASC_HOME,
+        dataStoreKey = PrefKey.SORT_ASC_HOME,
         defaultValue = true,
     )
 
     val sortAscBackup = PrefBoolean(
         dataStore = dataStore,
-        key = PrefKey.SORT_ASC_BACKUP,
+        dataStoreKey = PrefKey.SORT_ASC_BACKUP,
         defaultValue = true,
     )
 
     val sortAscRestore = PrefBoolean(
         dataStore = dataStore,
-        key = PrefKey.SORT_ASC_RESTORE,
+        dataStoreKey = PrefKey.SORT_ASC_RESTORE,
         defaultValue = true,
     )
 
     val mainFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.MAIN_FILTER_HOME,
+        dataStoreKey = PrefKey.MAIN_FILTER_HOME,
         defaultValue = MAIN_FILTER_USER,
         entries = possibleMainFilters, // not really, but shouldn't have an effect
     )
 
     val mainFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.MAIN_FILTER_BACKUP,
+        dataStoreKey = PrefKey.MAIN_FILTER_BACKUP,
         defaultValue = MAIN_FILTER_USER,
         entries = possibleMainFilters, // not really, but shouldn't have an effect
     )
 
     val mainFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.MAIN_FILTER_RESTORE,
+        dataStoreKey = PrefKey.MAIN_FILTER_RESTORE,
         defaultValue = MAIN_FILTER_USER,
         entries = possibleMainFilters, // not really, but shouldn't have an effect
     )
 
     val backupFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.BACKUP_FILTER_HOME,
+        dataStoreKey = PrefKey.BACKUP_FILTER_HOME,
         defaultValue = BACKUP_FILTER_DEFAULT,
         entries = batchModesSequence, // not really, but shouldn't have an effect
     )
 
     val backupFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.BACKUP_FILTER_BACKUP,
+        dataStoreKey = PrefKey.BACKUP_FILTER_BACKUP,
         defaultValue = BACKUP_FILTER_DEFAULT,
         entries = batchModesSequence, // not really, but shouldn't have an effect
     )
 
     val backupFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.BACKUP_FILTER_RESTORE,
+        dataStoreKey = PrefKey.BACKUP_FILTER_RESTORE,
         defaultValue = BACKUP_FILTER_DEFAULT,
         entries = batchModesSequence, // not really, but shouldn't have an effect
     )
 
     val installedFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.INSTALLED_FILTER_HOME,
+        dataStoreKey = PrefKey.INSTALLED_FILTER_HOME,
         defaultValue = InstalledFilter.ALL.ordinal,
         entries = InstalledFilter.entries.map { it.ordinal },
     )
 
     val installedFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.INSTALLED_FILTER_BACKUP,
+        dataStoreKey = PrefKey.INSTALLED_FILTER_BACKUP,
         defaultValue = InstalledFilter.ALL.ordinal,
         entries = InstalledFilter.entries.map { it.ordinal },
     )
 
     val installedFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.INSTALLED_FILTER_RESTORE,
+        dataStoreKey = PrefKey.INSTALLED_FILTER_RESTORE,
         defaultValue = InstalledFilter.ALL.ordinal,
         entries = InstalledFilter.entries.map { it.ordinal },
     )
 
     val launchableFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LAUNCHABLE_FILTER_HOME,
+        dataStoreKey = PrefKey.LAUNCHABLE_FILTER_HOME,
         defaultValue = LaunchableFilter.ALL.ordinal,
         entries = LaunchableFilter.entries.map { it.ordinal },
     )
 
     val launchableFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LAUNCHABLE_FILTER_BACKUP,
+        dataStoreKey = PrefKey.LAUNCHABLE_FILTER_BACKUP,
         defaultValue = LaunchableFilter.ALL.ordinal,
         entries = LaunchableFilter.entries.map { it.ordinal },
     )
 
     val launchableFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LAUNCHABLE_FILTER_RESTORE,
+        dataStoreKey = PrefKey.LAUNCHABLE_FILTER_RESTORE,
         defaultValue = LaunchableFilter.ALL.ordinal,
         entries = LaunchableFilter.entries.map { it.ordinal },
     )
 
     val updatedFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.UPDATED_FILTER_HOME,
+        dataStoreKey = PrefKey.UPDATED_FILTER_HOME,
         defaultValue = UpdatedFilter.ALL.ordinal,
         entries = UpdatedFilter.entries.map { it.ordinal },
     )
 
     val updatedFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.UPDATED_FILTER_BACKUP,
+        dataStoreKey = PrefKey.UPDATED_FILTER_BACKUP,
         defaultValue = UpdatedFilter.ALL.ordinal,
         entries = UpdatedFilter.entries.map { it.ordinal },
     )
 
     val updatedFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.UPDATED_FILTER_RESTORE,
+        dataStoreKey = PrefKey.UPDATED_FILTER_RESTORE,
         defaultValue = UpdatedFilter.ALL.ordinal,
         entries = UpdatedFilter.entries.map { it.ordinal },
     )
 
     val latestFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LATEST_FILTER_HOME,
+        dataStoreKey = PrefKey.LATEST_FILTER_HOME,
         defaultValue = LatestFilter.ALL.ordinal,
         entries = LatestFilter.entries.map { it.ordinal },
     )
 
     val latestFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LATEST_FILTER_BACKUP,
+        dataStoreKey = PrefKey.LATEST_FILTER_BACKUP,
         defaultValue = LatestFilter.ALL.ordinal,
         entries = LatestFilter.entries.map { it.ordinal },
     )
 
     val latestFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.LATEST_FILTER_RESTORE,
+        dataStoreKey = PrefKey.LATEST_FILTER_RESTORE,
         defaultValue = LatestFilter.ALL.ordinal,
         entries = LatestFilter.entries.map { it.ordinal },
     )
 
     val enabledFilterHome = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.ENABLED_FILTER_HOME,
+        dataStoreKey = PrefKey.ENABLED_FILTER_HOME,
         defaultValue = EnabledFilter.ALL.ordinal,
         entries = EnabledFilter.entries.map { it.ordinal },
     )
 
     val enabledFilterBackup = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.ENABLED_FILTER_BACKUP,
+        dataStoreKey = PrefKey.ENABLED_FILTER_BACKUP,
         defaultValue = EnabledFilter.ALL.ordinal,
         entries = EnabledFilter.entries.map { it.ordinal },
     )
 
     val enabledFilterRestore = PrefInt(
         dataStore = dataStore,
-        key = PrefKey.ENABLED_FILTER_RESTORE,
+        dataStoreKey = PrefKey.ENABLED_FILTER_RESTORE,
         defaultValue = EnabledFilter.ALL.ordinal,
         entries = EnabledFilter.entries.map { it.ordinal },
     )
 
-    val tagsFilterHome = StringSetPref(
+    val tagsFilterHome = PrefStringSet(
         dataStore = dataStore,
-        key = PrefKey.TAGS_FILTER_HOME,
+        dataStoreKey = PrefKey.TAGS_FILTER_HOME,
         defaultValue = emptySet(),
     )
 
-    val tagsFilterBackup = StringSetPref(
+    val tagsFilterBackup = PrefStringSet(
         dataStore = dataStore,
-        key = PrefKey.TAGS_FILTER_BACKUP,
+        dataStoreKey = PrefKey.TAGS_FILTER_BACKUP,
         defaultValue = emptySet(),
     )
 
-    val tagsFilterRestore = StringSetPref(
+    val tagsFilterRestore = PrefStringSet(
         dataStore = dataStore,
-        key = PrefKey.TAGS_FILTER_RESTORE,
+        dataStoreKey = PrefKey.TAGS_FILTER_RESTORE,
         defaultValue = emptySet(),
     )
 
